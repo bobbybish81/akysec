5864e0ac93960f116d38bd5149936c48cf9b594f
bobbybish81 <bobbybish81@hotmail.com>
2023-08-17 10:56:01 +0200

    sanity token added

--- src/sanity.ts
+++ src/sanity.ts
@@ -5,7 +5,7 @@ const sanityClient = createClient({
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2023-08-10', // use current date (YYYY-MM-DD) to target the latest API version
-  token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
+  token: process.env.REACT_APP_SANITY_SECRET_TOKEN // Only if you want to update content with the client
 });
 
 export default sanityClient
